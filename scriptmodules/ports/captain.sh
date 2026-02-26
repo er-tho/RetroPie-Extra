8d32b01771ff3491044acf5419a3cbf6a7a7df9e
erTho <robin.calmejane@gmail.com>
2026-02-26 23:15:02 +0100

    Updated for Trixie
    + Note added in the help message

--- scriptmodules/ports/captain.sh
+++ scriptmodules/ports/captain.sh
@@ -11,14 +11,23 @@
 
 rp_module_id="captain_s"
 rp_module_desc="Captain 'S' The Remake"
-rp_module_help="Controls: Arrow: Move, CTRL: Action, ENTER: Change character when get a sausage or change superpower when you are Captain S."
+rp_module_help="Controls: Arrow: Move, CTRL: Action, ENTER: Change character when get a sausage or change superpower when you are Captain S.
+
+Note: there is bug in the game where you can't change language once you set one. Fix it by deleting the file 'capitan.cfg' in ~/.capitan (reset the full configuration)"
 rp_module_licence="MIT https://raw.githubusercontent.com/jmcerrejon/PiKISS/master/LICENSE.md"
 rp_module_repo="file https://github.com/Exarkuniv/Rpi-pikiss-binary/raw/Master/captain_s.tar.gz"
 rp_module_section="exp"
 rp_module_flags="!armv6"
 
 function depends_captain_s() {
-    getDepends xorg liballegro4.4 libpng16-16
+    local depends=(xorg)
+	
+	if [[ "$__os_debian_ver" -ge 13 ]]; then
+		depends+=(liballegro4.4t64 libpng16-16t64)
+	else
+		depends+=(liballegro4.4 libpng16-16)
+	fi
+    getDepends "${depends[@]}"
 }
 
 function sources_captain_s() {
