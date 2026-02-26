4656e383be1ddfaf909d0a031ce4bbd685d3d06c
erTho <robin.calmejane@gmail.com>
2026-02-26 19:15:04 +0100

    Update bstone.sh
    Corrected script to make it work

--- scriptmodules/ports/bstone.sh
+++ scriptmodules/ports/bstone.sh
@@ -14,7 +14,7 @@ rp_module_id="bstone"
 rp_module_desc="Blake Stone - BStone a source port of Blake Stone: Aliens of Gold and Blake Stone: Planet Strike"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/bibendovsky/bstone/develop/LICENSE"
 rp_module_help="For Aliens of Gold registered version, replace the shareware files in $romdir/ports/bstone/aog/ to play. These files for the registered version are required: AUDIOHED.BS6, AUDIOT.BS6, EANIM.BS6, GANIM.BS6, IANIM.BS6, MAPHEAD.BS6, MAPTEMP.BS6, SANIM.BS6, VGADICT.BS6, VGAGRAPH.BS6, VGAHEAD.BS6, VSWAP.BS6."
-rp_module_repo="git https://github.com/bibendovsky/bstone.git master 0f01190"
+rp_module_repo="git https://github.com/bibendovsky/bstone.git master"
 rp_module_section="exp"
 rp_module_flags=""
 
@@ -27,16 +27,16 @@ function sources_bstone() {
 }
 
 function build_bstone() {
-    cd src
-    cmake .. -DCMAKE_BUILD_TYPE=Release
-    make
-    md_ret_require="$md_src"
+    mkdir build
+    cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$md_build/build/install"
+    cmake --build . --target install
+	md_ret_require="$md_build/build/install/bstone"
 }
 
 function install_bstone() {
-    md_ret_files=(
-       'src/src/bstone'
-    )
+	md_ret_files=(
+        'build/install/bstone'
+	)
 }
 
 function game_data_bstone() {
@@ -53,11 +53,10 @@ function configure_bstone() {
     addPort "$md_id" "bsaog" "Blake Stone - Aliens of Gold" "$md_inst/bstone --data_dir $romdir/ports/bstone/aog"	
 	addPort "$md_id" "bsps" "Blake Stone - Planet Strike" "$md_inst/bstone --data_dir $romdir/ports/bstone/ps"
 
-    #mkRomDir "ports/bstone"
     mkRomDir "ports/bstone/aog"
     mkRomDir "ports/bstone/ps"
-    mkdir $home/.local/share/bibendovsky
-    mkdir $home/.local/share/bibendovsky/bstone
+    mkUserDir $home/.local/share/bibendovsky
+    mkUserDir $home/.local/share/bibendovsky/bstone
 
     moveConfigDir "$home/.local/share/bibendovsky/bstone" "$md_conf_root/bibendovsky/bstone"
 
