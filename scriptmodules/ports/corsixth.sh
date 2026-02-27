9f461609ec643cb2597b08f7ad8ac30a95a267a0
erTho <robin.calmejane@gmail.com>
2026-02-27 03:47:29 +0100

    Update corsixth.sh
    - Updated to last version installation from official repo.
    - Corrected some missing permissions
    - Had to disable Midi handling (introduced by last game version) during compilation as it wasn't working despite librtmidi installed.

--- scriptmodules/ports/corsixth.sh
+++ scriptmodules/ports/corsixth.sh
@@ -12,12 +12,12 @@
 rp_module_id="corsixth"
 rp_module_desc="CorsixTH - Theme Hospital Engine"
 rp_module_licence="MIT https://raw.githubusercontent.com/CorsixTH/CorsixTH/master/LICENSE.txt"
-rp_module_help="Mouse or mouse emulation through xboxdrv is required. You need to copy your Theme Hospital game data into $romdir/ports/$md_id/ and when starting up the game for the first time, select the directory. The colors and fonts could have bad colors here making it difficult."
+rp_module_help="Mouse or mouse emulation through xboxdrv is required. You need to copy your Theme Hospital game data into $romdir/ports/corsixth/ and when starting up the game for the first time, select the directory. The colors and fonts could have bad colors here making it difficult."
 rp_module_section="exp"
-rp_module_flags="!x86 !mali"
+rp_module_flags="noinstclean !x86 !mali"
 
 function depends_corsixth() {
-    local depends=(cmake liblua5.2-0 liblua5.2-dev libsdl2-dev libsdl2-mixer-dev timidity libfreetype6-dev lua-filesystem lua-lpeg libavcodec-dev libavformat-dev libavdevice-dev libavutil-dev libswscale-dev libpostproc-dev libavfilter-dev matchbox freepats)
+    local depends=(cmake liblua5.3-0 liblua5.3-dev liblua5.3-0-dbg libsdl2-dev libsdl2-mixer-dev fluidsynth libfreetype6-dev lua-filesystem lua-lpeg doxygen ffmpeg libavcodec-dev libavformat-dev libavdevice-dev libavutil-dev libswscale-dev libpostproc-dev libavfilter-dev libswresample-dev librtmidi-dev matchbox)
     isPlatform "32bit" && depends+=(libavresample-dev)
    getDepends "${depends[@]}"
 }
@@ -27,29 +27,29 @@ function sources_corsixth() {
 }
 
 function build_corsixth() {
-    cmake . -DWITH_LIBAV=ON -DCMAKE_INSTALL_PREFIX:PATH="$md_inst"
-    cd ./CorsixTH
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX:PATH="$md_inst" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Ofast -DNDEBUG" -DWITH_MIDI_DEVICE=OFF ..
     make
-    md_return_require="$md_build/CorsixTH/CorsixTH"
-    cd "$md_build"
+    md_return_require="$md_build/build/CorsixTH"
+	
 }
 
 function install_corsixth() {
+    cd "$md_build/build/CorsixTH"
     make install
 }
 
 function game_data_corsixth() {
-    mkRomDir "ports"
-    mkRomDir "ports/$md_id"
     if [[ ! -f "$romdir/ports/$md_id/HOSP.exe" ]]; then
         downloadAndExtract "https://archive.org/download/HOSP_zip/HOSP.zip" "$romdir/ports/$md_id"
     fi
+	chown -R $__user:$__group "$romdir/ports/$md_id"
 }
 
 function configure_corsixth() {
     mkRomDir "ports"
     mkRomDir "ports/$md_id"
-    #mkdir "$home/.config/CorsixTH"
     moveConfigDir "$home/.config/CorsixTH" "$md_conf_root/$md_id"
     addPort "$md_id" "corsixth" "CorsixTH - Theme Hospital Engine" "$md_inst/bin/corsix.sh"
 
@@ -67,5 +67,6 @@ theme_hospital_install = [[$romdir/ports/$md_id]]
 _EOF_
 
      chmod +x "$md_inst/bin/corsix.sh"
+	 chown $__user:$__group "$md_conf_root/$md_id/config.txt"
     [[ "$md_mode" == "install" ]] && game_data_corsixth
 }
